c851e7bd442034fb8ddfb1dfc0066e94e1a36dba
AL <javachina@foxmail.com>
2020-01-21 02:38:56 +0800

    initial commit

---
+++ src/Id.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace al\helper;
+
+/**
+ * 对数据自增主键进行追加长度的混淆类
+ * Class Id
+ * @package al\helper
+ */
+class Id
+{
+    /**
+     * 把数据自增主键转换为ID
+     * @param $key
+     * @return int
+     */
+    public static function key2id($key)
+    {
+        return 0;
+    }
+
+    /**
+     * 把ID还原为数据自增主键
+     * @param $id
+     * @return int
+     */
+    public static function id2key($id)
+    {
+        return 0;
+    }
+
+}
