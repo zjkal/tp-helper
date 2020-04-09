88e9ae9bc3c6c7dacaef8da0a538fafb3177cca2
AL <javachina@foxmail.com>
2020-04-10 01:02:35 +0800

    时间助手类

--- tests/DateTest.php
+++ tests/DateTest.php
@@ -6,3 +6,9 @@ $datetime = '2020-04-10 22:22:22';
 var_dump(\al\helper\Date::isToday($datetime));
 var_dump(\al\helper\Date::isThisMonth($datetime));
 var_dump(\al\helper\Date::isThisYear($datetime));
+
+$datetime = 1586451741;
+
+var_dump(\al\helper\Date::isToday($datetime));
+var_dump(\al\helper\Date::isThisMonth($datetime));
+var_dump(\al\helper\Date::isThisYear($datetime));
