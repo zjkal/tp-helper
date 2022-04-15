f964984e225f92581aad6cdc7d100071d4208e4b
zjkal <javachina@foxmail.com>
2022-04-15 15:00:29 +0800

    增加了返回两个日期相差天数的方法

--- src/Time.php
+++ src/Time.php
@@ -28,6 +28,28 @@ class Time
         }
     }
 
+    //将任意时间类型的参数转为时间戳
+    public static function toTimestamp($datetime): int
+    {
+        if (!$datetime) {
+            throw new \InvalidArgumentException('Param datetime must be a timestamp or a string time');
+        }
+
+        $start = strtotime('1970-01-01 07:00:00'); // 0
+        $end = strtotime('2038-01-19 03:14:07'); // 2147454847
+        //判断是否为时间戳
+        if (is_numeric($datetime) && $datetime <= $end && $datetime >= $start) {
+            return intval($datetime);
+        } else {
+            $timestamp = strtotime($datetime);
+            if ($timestamp) {
+                return $timestamp;
+            } else {
+                throw new \InvalidArgumentException('Param datetime must be a timestamp or a string time');
+            }
+        }
+    }
+
     /**
      * 返回到今天晚上零点之前的秒数
      * @return false|int
@@ -65,12 +87,10 @@ class Time
      * @param int $max_days
      * @return false|string
      */
-    public static function friendly_date($time, int $max_days = 365, string $format = 'Y年m月d日')
+    public static function friendly_date($time, int $max_days = 365, string $format = 'Y年m月d日', string $lang = 'zh')
     {
-        if (!$time) {
-            return '';
-        }
-        $time = self::is_timestamp($time) ? $time : strtotime($time);
+
+        $time = self::toTimestamp($time);
         $now_time = time();
         if ($time > $now_time) {
             return date($format, $time);
@@ -87,19 +107,98 @@ class Time
         if ($diff_days >= $max_days) {
             return date($format, $time);
         } elseif ($diff_days >= 3) {
-            return $diff_days . '天前';
-        } elseif ($diff_days >= 2) {
-            return '前天';
+            return $diff_days . ($lang == 'zh' ? '天前' : ' days ago');
         } elseif ($diff_days >= 1) {
-            return '昨天';
+            return $lang == 'zh' ? '昨天' : 'yesterday';
         } elseif ($diffs >= 3600) {
-            return floor($diffs / 3600) . '小时前';
+            return floor($diffs / 3600) . ($lang == 'zh' ? '小时前' : ' hours ago');
         } elseif ($diffs >= 60) {
-            return floor($diffs / 60) . '分钟前';
+            return floor($diffs / 60) . ($lang == 'zh' ? '分钟前' : ' minutes ago');
         } elseif ($diffs >= 10) {
-            return $diffs . '秒前';
+            return $diffs . ($lang == 'zh' ? '秒前' : ' seconds ago');
         } else {
-            return '刚刚';
+            return $lang == 'zh' ? '刚刚' : 'just';
         }
+    }
+
+    /**
+     * 判断日期是否为今天
+     * @param string|int $datetime
+     * @return bool
+     */
+    public static function isToday($datetime): bool
+    {
+        $timestamp = self::toTimestamp($datetime);
+        if (date('Y-m-d', $timestamp) == date('Y-m-d')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 判断日期是否为本周
+     * @param string|int $datetime
+     * @return bool
+     */
+    public static function isThisWeek($datetime): bool
+    {
+        $week_start = strtotime(date('Y-m-d 00:00:00', strtotime('this week')));
+        $week_end = strtotime(date('Y-m-d 23:59:59', strtotime('last day next week')));
+        $timestamp = self::toTimestamp($datetime);
+        if ($timestamp >= $week_start && $timestamp <= $week_end) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    /**
+     * 判断日期是否为本月
+     * @param string|int $datetime
+     * @return bool
+     */
+    public static function isThisMonth($datetime): bool
+    {
+        $timestamp = self::toTimestamp($datetime);
+        if (date('Y-m', $timestamp) == date('Y-m')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 判断日期是否为今年
+     * @param string|int $datetime
+     * @return bool
+     */
+    public static function isThisYear($datetime): bool
+    {
+        $timestamp = self::toTimestamp($datetime);
+        if (date('Y', $timestamp) == date('Y')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 返回两个日期相差天数(如果只传入一个日期,则与当天时间比较)
+     * @param $datetime
+     * @param $new_datetime
+     * @return int
+     */
+    public static function diffDays($datetime, $new_datetime = null): int
+    {
+        $datetime = date('Y-m-d', self::toTimestamp($datetime));
+        if ($new_datetime) {
+            $new_datetime = date('Y-m-d', self::toTimestamp($new_datetime));
+        } else {
+            $new_datetime = date('Y-m-d');
+        }
+
+        return date_diff(date_create($datetime), date_create($new_datetime))->days;
     }
 }
--- tests/TimeTest.php
+++ tests/TimeTest.php
@@ -6,4 +6,29 @@ echo \al\helper\Time::secondEndToday() . PHP_EOL;
 echo \al\helper\Time::secondDay(7) . PHP_EOL;
 echo \al\helper\Time::secondWeek(4) . PHP_EOL;
 echo \al\helper\Time::friendly_date('2022-3-2 10:15:33') . PHP_EOL;
-echo \al\helper\Time::friendly_date(1646186290, 365, 'Y-m-d') . PHP_EOL;
+echo \al\helper\Time::friendly_date(1646186290, 365, 'Y-m-d', 'en') . PHP_EOL;
+
+$datetime = '2020-04-10 22:22:22';
+
+var_dump(\al\helper\Time::isToday($datetime));
+var_dump(\al\helper\Time::isThisMonth($datetime));
+var_dump(\al\helper\Time::isThisYear($datetime));
+var_dump(\al\helper\Time::isThisWeek($datetime));
+
+$datetime = 1586451741;
+
+var_dump(\al\helper\Time::isToday($datetime));
+var_dump(\al\helper\Time::isThisMonth($datetime));
+var_dump(\al\helper\Time::isThisYear($datetime));
+var_dump(\al\helper\Time::isThisWeek($datetime));
+
+$datetime = 'Apr 11, 2020';
+
+var_dump(\al\helper\Time::isToday($datetime));
+var_dump(\al\helper\Time::isThisMonth($datetime));
+var_dump(\al\helper\Time::isThisYear($datetime));
+var_dump(\al\helper\Time::isThisWeek($datetime));
+
+var_dump(\al\helper\Time::diffDays('2022-4-10 23:01:11', 'Apr 11, 2022')) . PHP_EOL;
+//如果只传入一个参数,则与当前时间比较
+var_dump(\al\helper\Time::diffDays(1586451741)) . PHP_EOL;
