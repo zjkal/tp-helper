8f6dd3a49b3a6e68aad9fff4a8e1ed1b158b6264
zjkal <javachina@foxmail.com>
2022-04-26 14:44:31 +0800

    完善文档

--- src/Time.php
+++ src/Time.php
@@ -30,6 +30,7 @@ class Time
 
     /**
      * 将任意时间类型的参数转为时间戳
+     * 请注意 m/d/y 或 d-m-y 格式的日期，如果分隔符是斜线（/），则使用美洲的 m/d/y 格式。如果分隔符是横杠（-）或者点（.），则使用欧洲的 d-m-y 格式。为了避免潜在的错误，您应该尽可能使用 YYYY-MM-DD 格式或者使用 date_create_from_format() 函数。
      * @param int|string $datetime 要转换为时间戳的字符串或数字
      * @return int 时间戳
      */
